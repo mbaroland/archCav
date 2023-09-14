1c1523cc67ef0acadd9c5e51bc2a6fa8496ecc67
mbaroland <mbaroland.kumena@ifnti.com>
2023-09-14 16:49:39 +0000

    permission

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -3,7 +3,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;class CreateAdminUserSeeder extends Seeder
+use Spatie\Permission\Models\Permission;
+class CreateAdminUserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
