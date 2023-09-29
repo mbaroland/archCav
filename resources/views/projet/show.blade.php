f15098d7c61f16afdc162bc57418bc2faf4282fe
mbaroland <rolandkumena@gmail.com>
2023-09-29 17:25:06 +0000

    archcavFin

--- resources/views/projet/show.blade.php
+++ resources/views/projet/show.blade.php
@@ -3,33 +3,6 @@
 
 <div class="p-4 sm:ml-64">
     <div class="p-4 border-0 border-gray-200 rounded-lg dark:border-gray-700 mt-16">
-        <div class="p-4 border-0 border-gray-200 rounded-lg dark:border-gray-700">
-            <h1 class="text-2xl font-semibold text-gray-900 dark:text-white">{{ $projet->titre_projet }}</h1>
-            <p class="text-gray-600 dark:text-gray-400 mt-2">{{ $projet->objectif_global }}</p>
-
-            <p class="text-gray-600 dark:text-gray-400 mt-2">{{ $projet->objectif_specifiques }}</p>
-
-
-            <ul class="mt-4">
-                <li><strong>Financement:</strong> {{ $projet->financement }}</li>
-                <li><strong>Budget:</strong> {{ $projet->budjet }} FCFA</li>
-                <li><strong>Zone:</strong> {{ $projet->zone }}</li>
-                <li><strong>Durée:</strong> {{ $projet->find_duration() }} mois</li>
-                <!-- Ajoutez d'autres détails du projet ici -->
-            </ul>
-
-            <!-- Ajoutez d'autres sections ou détails du projet selon vos besoins -->
-        </div>
-    </div>
-</div>
-
-@endsection
-
-@extends('dashboard')
-@section('content')
-
-<div class="p-4 sm:ml-64">
-    <div class="p-4 border-0 border-gray-200 rounded-lg dark:border-gray-700 mt-16">
        
             
             
@@ -59,6 +32,14 @@
                                         
                                     </div>
                                 </div>
+
+                                <ul class="mt-4">
+                <li><strong>Financement:</strong> {{ $projet->financement }}</li>
+                <li><strong>Budget:</strong> {{ $projet->budjet }} FCFA</li>
+                <li><strong>Zone:</strong> {{ $projet->zone }}</li>
+                <li><strong>Durée:</strong> {{ $projet->find_duration() }} mois</li>
+                <!-- Ajoutez d'autres détails du projet ici -->
+            </ul>
                     
                             </div>
                     </div>
