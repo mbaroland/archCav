9499016c9ef9ca561ee5685cf4f7902f84b1af71
mbaroland <mbaroland.kumena@ifnti.com>
2023-10-14 11:39:08 +0000

    back

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,25 +1,23 @@
 @extends('accueil')
 @section('content1')
-    <div class="p-4 border-0 border-gray-200 rounded-lg dark:border-gray-700 mt-16">
+    <div class=" bg-white p-4 border-0 border-gray-200 rounded-lg dark:border-gray-700 mt-16">
     <div class="flex justify-center">
         <div class="w-full md:w-2/3">
             <div class="my-4">
-                <h2 class="text-lg font-semibold">{{ __('Show User') }}</h2>
+                <h2 class="text-lg font-semibold">{{ __('PERSONNE') }}</h2>
             </div>
-            <div class="flex items-center my-4">
-                <div class="flex-grow">
-                    <a href="{{ route('users.index') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        {{ __('Back') }}
-                    </a>
-                </div>
-            </div>
+            
             <div class="my-4">
                 <div class="card shadow">
                     <div class="card-body">
                         <div class="col-span-12">
                             <div class="mb-4">
-                                <strong class="font-semibold">{{ __('Name') }}:</strong>
-                                {{ $user->name }}
+                                <strong class="font-semibold">{{ __('Nom') }}:</strong>
+                                {{ $user->name }} 
+                                <br>
+                                <strong class="font-semibold">{{ __('Prenom') }}:</strong>
+                                {{ $user->prenom }}
+
                             </div>
                         </div>
                         <div class="col-span-12">
@@ -30,7 +28,7 @@
                         </div>
                         <div class="col-span-12">
                             <div class="mb-4">
-                                <strong class="font-semibold">{{ __('Roles') }}:</strong>
+                                <strong class="font-semibold">{{ __('Role') }}:</strong>
                                 @if (!empty($user->getRoleNames()))
                                     @foreach ($user->getRoleNames() as $v)
                                         <span class="badge badge-success bg-green-500 text-white px-2 py-1 rounded-full">{{ $v }}</span>
@@ -39,6 +37,13 @@
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div class="flex items-center my-4">
+                <div class="flex-grow">
+                    <a href="{{ route('users.index') }}"  class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full">
+                        {{ __('Retour') }}
+                    </a>
                 </div>
             </div>
         </div>
