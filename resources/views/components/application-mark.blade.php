afcac2ec05895e4c8dad85febe5a08c43f684d20
bakiscofield <bakitchassei@gmail.com>
2023-11-24 13:46:32 +0000

    zone

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,5 +1,5 @@
 
 <a href="#" class="flex items-center">
-  <img src="{{ asset('logo.png') }}" class="h-8 mr-3" alt="ArchCAV Logo" />
-  <span class="self-center text-2xl font-semibold whitespace-nowrap text-blue-700">ArchCAV</span>
+  <img src="{{ asset('logo.png') }}" class="h-8 mr-3" alt="ArchiCAV Logo" />
+  <span class="self-center text-2xl font-semibold whitespace-nowrap text-blue-700">ArchiCAV</span>
 </a>
